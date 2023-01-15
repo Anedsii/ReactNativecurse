02edc2d2a3f082be76447af29ed5de52031f85d1
Anedsii <angelacilez@gmail.com>
2023-01-15 20:41:10 -0300

    textinput_y_button

--- App.js
+++ App.js
@@ -3,7 +3,15 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-     <Text>Hola soy anshu</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} 
+        placeholder="add nombre"
+        autoCorrect={false}
+        autoCapitalize='none'
+        
+        ></TextInput>
+        <Button style={styles.button} title="add" color="pink"/>
+      </View>
     </View>
   );
 }
@@ -12,8 +20,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent:'center',
-    alignItems:'center'
   },
+  inputContainer:{
+    marginTop:50,
+    flexDirection:'row',
+    marginHorizontal:20,
+    justifyContent:'space-between'
+  },
+  input:{
+    borderBottomColor:'#C5979D',
+    borderBottomWidth:1,
+    color:'pink',
+    height:40,
+    width:'75%',
+    
+
+  },
+  
  
 });
